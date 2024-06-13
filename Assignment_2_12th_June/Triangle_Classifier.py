386da23d4c970768a87507d8baa9918db2675366
amitbidwai28 <amitbidwai281@gmail.com>
2024-06-13 19:38:30 +0530

    Triangle program added

--- Assignment_2_12th_June/Triangle_Classifier.py
+++ Assignment_2_12th_June/Triangle_Classifier.py
@@ -1,7 +1,7 @@
 def Classifir():
-    side1 = float(input("Enter the first side"))
-    side2 = float(input("Enter the second side"))
-    side3 = float(input("Enter the third side"))
+    side1 = float(input("Enter the first side: "))
+    side2 = float(input("Enter the second side: "))
+    side3 = float(input("Enter the third side: "))
     if (side1 == side2 and side2 == side3 and side3 == side1):
         print("Triangle is equilaterl")
     elif (side1 == side2) or (side2 == side3) or (side3 == side1):
