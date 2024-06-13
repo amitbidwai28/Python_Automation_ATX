4ab93834ef85bffecf059b7beec7d407a8f5174c
amitbidwai28 <amitbidwai281@gmail.com>
2024-06-13 20:19:18 +0530

    Factorial program added

--- Assignment_2_12th_June/factorial_no.py
+++ Assignment_2_12th_June/factorial_no.py
@@ -1,5 +1,9 @@
-fact = int(input("Enter the nu. for factorial: "))
-num = 1
-for i in range(1, fact + 1):
-    num = num * i
-print("Factorial of the given no. is", num)
+def factorial():
+    fact = int(input("Enter the nu. for factorial: "))
+    num = 1
+    for i in range(1, fact + 1):
+        num = num * i
+    print("Factorial of the given no. is", num)
+
+
+factorial()
