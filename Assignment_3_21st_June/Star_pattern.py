b1384a37565b736f401d391363dfa2e0d59226a0
amitbidwai28 <amitbidwai281@gmail.com>
2024-06-21 22:31:25 +0530

    Star pattern added

---
+++ Assignment_3_21st_June/Star_pattern.py
@@ -0,0 +1,9 @@
+def star_pt(num):
+    for i in range(num):
+        for j in range(i+1):
+            print("*", end=" ")
+        print("\r")
+
+
+val = int(input("Enter the value: "))
+star_pt(val)
