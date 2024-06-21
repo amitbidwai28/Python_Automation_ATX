1fcf3f9021cd2c287843ec1f05e22799dc5479f7
amitbidwai28 <amitbidwai281@gmail.com>
2024-06-21 23:04:52 +0530

    Right pascal triangle added

---
+++ Assignment_3_21st_June/Left_pascal_triangle.py
@@ -0,0 +1,10 @@
+num = 5
+for i in range(num-1):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print("\r")
+num1 = 5
+for i in range(num1):
+    for j in range(i, num1):
+        print("*", end=" ")
+    print("\r")
